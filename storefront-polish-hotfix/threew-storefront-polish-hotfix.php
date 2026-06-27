082e727cff2e58321a756ed4a960e8448f265a67
Alan Wang <alanw707@gmail.com>
2026-06-27 08:36:04 -0700

    Set Modern Cart floating position via option

--- storefront-polish-hotfix/threew-storefront-polish-hotfix.php
+++ storefront-polish-hotfix/threew-storefront-polish-hotfix.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: 3W Storefront Polish Hotfix
  * Description: Small design and accessibility polish fixes for the 3W Distributing Porto storefront homepage.
- * Version: 1.2.111
+ * Version: 1.2.113
  * Author: 3W Distributing
  */
 
@@ -10,11 +10,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'THREEW_STOREFRONT_POLISH_VERSION', '1.2.111' );
+define( 'THREEW_STOREFRONT_POLISH_VERSION', '1.2.113' );
 
 add_action(
 	'init',
 	static function () {
+		if ( get_option( 'threew_storefront_moderncart_position_configured' ) !== 'yes' ) {
+			$moderncart_floating_settings = get_option( 'moderncart_floating', [] );
+
+			if ( ! is_array( $moderncart_floating_settings ) ) {
+				$moderncart_floating_settings = [];
+			}
+
+			$moderncart_floating_settings['floating_cart_position'] = 'bottom-left';
+			update_option( 'moderncart_floating', $moderncart_floating_settings, false );
+			update_option( 'threew_storefront_moderncart_position_configured', 'yes', false );
+		}
+
 		if ( get_option( 'threew_storefront_polish_version' ) === THREEW_STOREFRONT_POLISH_VERSION ) {
 			return;
 		}
